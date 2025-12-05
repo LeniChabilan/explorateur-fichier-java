98d5d2f6ece771d5bb9a8b40322b7415405bff02
ChabilanLeni <chabilan.leni@gmail.com>
2025-12-05 11:23:05 +0100

    Added ExplorerApp.java

--- src/com/esiea/pootd2/ExplorerApp.java
+++ src/com/esiea/pootd2/ExplorerApp.java
@@ -1,1 +1,12 @@
-// Todo
+package com.esiea.pootd2;
+
+import com.esiea.pootd2.controllers.ExplorerController;
+import com.esiea.pootd2.interfaces.TextInterface;
+
+public class ExplorerApp {
+    public static void main(String[] args) {
+        ExplorerController controller = new ExplorerController();
+        TextInterface ui = new TextInterface(controller);
+        ui.run();
+    }
+}
