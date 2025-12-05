d236e5e047fe995fda730f533aca7716396908a4
ChabilanLeni <chabilan.leni@gmail.com>
2025-12-05 16:47:53 +0100

    updated

--- src/com/esiea/pootd2/ExplorerApp.java
+++ src/com/esiea/pootd2/ExplorerApp.java
@@ -2,7 +2,7 @@ package com.esiea.pootd2;
 
 import com.esiea.pootd2.controllers.ExplorerController;
 import com.esiea.pootd2.interfaces.TextInterface;
-//import com.esiea.pootd2.interfaces.HttpInterface;
+import com.esiea.pootd2.interfaces.HttpInterface;
 
 
 /**
@@ -11,11 +11,34 @@ import com.esiea.pootd2.interfaces.TextInterface;
 public class ExplorerApp {
     /**
      * Main method to start the Explorer application.
-     * @param args
+     * @param args command line arguments
+     *             - Use "http" to start with HTTP interface
+     *             - Use "text" to start with text interface
      */
     public static void main(String[] args) {
+        // Check if an argument is provided
+        if (args.length == 0) {
+            System.err.println("Error: You must specify an interface type.");
+            System.exit(1);
+        }
+        
         ExplorerController controller = new ExplorerController();
-        TextInterface ui = new TextInterface(controller);
-        ui.run();
+        String interfaceType = args[0].toLowerCase();
+        
+        switch (interfaceType) {
+            case "http":
+                HttpInterface httpUi = new HttpInterface(controller);
+                httpUi.run();
+                break;
+                
+            case "text":
+                TextInterface textUi = new TextInterface(controller);
+                textUi.run();
+                break;
+                
+            default:
+                System.err.println("Error: Invalid interface type '" + args[0] + "'");
+                System.exit(1);
+        }
     }
 }
